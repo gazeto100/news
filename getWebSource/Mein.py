5ea0e097a3eaa37c50f87af0c68eda06d99eaf91
Andrei Terziev <andrei.terziev@egt-bg.com>
2019-08-23 14:36:19 +0300

    delete and add new staf

--- getWebSource/Mein.py
+++ getWebSource/Mein.py
@@ -1,31 +1,42 @@
 import requests
-sites = [
-    'http://www.dnes.bg',
-    'http://www.vesti.bg',
-    'http://www.novini.bg',
-    'http://www.actualno.bg',
-    'http://www.btvnovinite.bg',
-    'http://www.nova.bg',
-]
-for url in sites:
-    index = sites.index(url)
-    r = requests.get(url)
-    page_source = r.text
-    page_source = page_source.split('\n')
 
+from bs4 import BeautifulSoup
 
-    print("\nURL:", url)
-    print("--------------------------------------")
-    # print the first five lines of the page source
-    f = open(str(index) + ".html", "w")
-    for row in page_source[:2000]:
-        if f.mode == 'w':
-            f.write(row)
-            f.write('\n')
-        print(row)
-    f.close()
-    print("--------------------------------------")
+page = requests.get('http://dnes.bg')
+soup = BeautifulSoup(page.content, 'html.parser')
 
+week = soup.find(class_='homepage')
+item = week.find_all(class_='first')
+#print(item)
 
+link = item[0].find('a').get('href')
+link1 = item[1].find('a').get('href')
 
+title = item[0].get_text()
+title1 = item[1].get_text()
 
+img = item[0].find('img').get('src')
+img1 = item[1].find('img').get('src')
+
+#print(title.strip())
+#print(title1.strip())
+
+#print('https://dnes.bg'+link)
+#print('https://dnes.bg'+link1)
+
+#print(img)
+#print(img1)
+
+img_title_1 = []
+
+dnesitem = week.find_all(class_='news-item')
+#print(dnesitem)
+
+for x in range(10):
+    img_title_1.append(dnesitem[x].find('span').get_text())
+    img_title_1.append(dnesitem[x].find('img').get('src'))
+    img_title_1.append(dnesitem[x].find('a').get('href'))
+    img_title_1.append(dnesitem[x].find(class_='news-subtitle').get_text())
+
+for x in range(len(img_title_1)):
+    print(img_title_1[x])
--- getWebSource/getNews.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-  var x = document.getElementsByTagName("H1")[0].getAttribute("class");
-  document.getElementById("demo").innerHTML = x;
-}
